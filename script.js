5360f4b6f53a97bb8cdaf737ee566eb3dbb827ba
jevgenijtimoscenko <jevgenij.timoscenko@gmail.com>
2025-10-17 21:18:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const blogList = document.getElementById("blog-list");
-  if (!blogList) return;
+  const posts = document.querySelectorAll(".blog-post");
+  posts.forEach(post => post.classList.add("fade-in"));
 
-  const posts = blogList.querySelectorAll(".blog-post");
-
-  posts.forEach(post => {
-    post.classList.add("fade-in"); // для эффекта
-  });
-
-  const elements = document.querySelectorAll(".fade-in");
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) entry.target.classList.add("visible");
     });
   }, { threshold: 0.1 });
 
-  elements.forEach(el => observer.observe(el));
+  posts.forEach(el => observer.observe(el));
 });
